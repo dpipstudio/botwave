7abc613b2248ef70db95f2ecaa4dd9baee3a89c5
douxxtech <douxx@douxx.tech>
2026-08-04 23:57:24 +0200

    client: a somewhat working version

--- client/client.py
+++ client/client.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 import os
 from pathlib import Path
@@ -6,11 +7,16 @@ import sys
 # using this to access to the shared dir files
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+from shared.alsa import Alsa
+from shared.cat import check
 from shared.env import Env
 from shared.logger import Log
 from shared.protocol import Commands, ProtocolParser
+from shared.pw_monitor import PWM
 from shared.registry import Registry, UpperException
+from shared.syscheck import check_requirements
 from shared.tips import TipEngine
+from shared.version import check_for_updates
 
 class BotWaveClient:
     def __init__(self):
@@ -21,12 +27,21 @@ class BotWaveClient:
         self.running = False
         self.registered = False
         self.client_id = None
+        self.piwave = None
+        self.piwave_monitor = PWM()
+        self.broadcasting = False
+        self.broadcast_start_time = None
+        self.current_file = None
+        self.stream_task = None
+        self.stream_active = False
+        self.feed_task = None
 
         self.registry = Registry(self)
         self.tips = TipEngine(is_server=False)
+        self.alsa = Alsa()
 
     async def handle_message(self, message: str):
-        Log.print(message)
+        Log.debug(message)
 
         parsed = ProtocolParser.parse_command(message)
         cmd = parsed['command']
@@ -35,26 +50,95 @@ class BotWaveClient:
             found = False
 
         else:
-            found = await self.registry.dispatch(cmd, parsed=parsed)
+            try:
+                found = await self.registry.dispatch(cmd, parsed=parsed)
+
+            except UpperException:
+                # upperexception: stop request, dont ask why
+                await self.ws_client.disconnect()
+                return
         
         if not found:
             Log.warning(f"Unknown command: {cmd}")
             await self.proto.reply(parsed, Commands.ERROR, message=f"Unknown command: {cmd}. Perhaps a protocol mismatch?")
 
 
+# startup helpers
+def set_prio(key, cli_value, default, immutable=False):
+    if cli_value is not None:
+        Env.set(key, str(cli_value), immutable=immutable)
+
+    elif not Env.get(key, False) and default is not None:
+        Env.set(key, str(default), immutable=immutable)
+
+def check_updates():
+    Log.info("Checking for software updates...")
+
+    try:
+        _, latest_ver = check_for_updates()
+
+        if latest_ver:
+            Log.update(f"A newer version of BotWave is available ({latest_ver})")
+            Log.update(f"Update by running 'bw-update --to {latest_ver}' in your shell")
+
+        else:
+            Log.success("You are using the latest version")
+
+    except Exception as e:
+        Log.warning("Unable to check for updates (continuing anyway)")
+
+# entry point
 async def main():
+    Log.header("BotWave Client")
+
+    parser = argparse.ArgumentParser(prog="bw-client", description='BotWave Client')
+    parser.add_argument('server_host', nargs='?', help='Server hostname/IP')
+    parser.add_argument('--port', type=int, default=None, help='Server port')
+    parser.add_argument('--fhost', help='File transfer server hostname/IP (defaults to server_host)')
+    parser.add_argument('--fport', type=int, default=None, help='File transfer (HTTP) port')
+    parser.add_argument('--upload-dir', default=None, help='Uploads directory')
+    parser.add_argument('--pk', help='Passkey for authentication')
+    parser.add_argument('--skip-checks', dest='skip_checks', action=argparse.BooleanOptionalAction, default=None, help='Skip update and requirements checks')
+    parser.add_argument('--talk', action=argparse.BooleanOptionalAction, default=None, help='Makes PiWave (broadcast manager) output logs visible.')
+    parser.add_argument('--config', type=str, help='Path to a config file to load into environment')
+    args = parser.parse_args()
+
+    if args.config:
+        Env.load(args.config) # will silently drop if file doesn't exist
+
+    if args.server_host:
+        Env.set("SERVER_HOST", args.server_host, immutable=True)
+        
+    elif not Env.get("SERVER_HOST", False):
+        try:
+            Env.set("SERVER_HOST", input("Server hostname/IP: ").strip(), immutable=True)
+
+        except Exception:
+            return
+
+    set_prio("SERVER_PORT", args.port, 9938, immutable=True)
+    set_prio("FHOST", args.fhost, Env.get("SERVER_HOST"), immutable=True)
+    set_prio("FPORT", args.fport, 9921, immutable=True)
+    set_prio("UPLOAD_DIR", args.upload_dir, '/opt/BotWave/uploads/')
+    set_prio("TALK", args.talk, False)
+    set_prio("SKIP_CHECKS", args.skip_checks, False)
+
+    if args.pk:
+        Env.set("PASSKEY", args.pk, immutable=True)
+
+    if not Env.get_bool("SKIP_CHECKS"):
+        check_requirements()
+        check_updates()
+
+
     client = BotWaveClient()
     client.registry.from_dir(Path(__file__).resolve().parent / "ops")
-
-    Env.set("SERVER_HOST", "localhost")
-    Env.set("SERVER_PORT", "9938")
     
     try:
         await client.registry.dispatch("client_connect")
 
     except UpperException:
-        #TODO: await client.registry.dispatch("client_stop")
-        sys.exit(1)
+        await client.registry.dispatch("client_stop")
 
     client.running = True
     client.tips.start()
@@ -65,9 +149,11 @@ async def main():
     except (KeyboardInterrupt, asyncio.CancelledError):
         Log.warning("Shutting down...")
 
+    except Exception as e:
+        Log.error(f"Error: {e}")
+
     finally:
-        #TODO: await client.registry.dispatch("client_stop")
-        sys.exit(1)
+        await client.registry.dispatch("client_stop")
 
 if __name__ == "__main__":
     asyncio.run(main())
