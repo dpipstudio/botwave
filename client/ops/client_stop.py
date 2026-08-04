81329a94f36740ff14f14693c230cc944d04a94b
douxxtech <douxx@douxx.tech>
2026-08-04 17:07:00 +0200

    client/ops/client_stop.py: internal op to cleanly stop the client (similar to the local's "exit" command)

---
+++ client/ops/client_stop.py
@@ -0,0 +1,25 @@
+from shared.logger import Log
+from shared.ops import GeneralOp
+from shared.protocol import Commands
+
+class ClientStopOp(GeneralOp):
+    commands = {"client_stop": "stop"}
+
+    async def stop(self):
+        self.owner.running = False
+
+        if self.owner.broadcasting:
+            await self.registry.dispatch(Commands.STOP)
+
+        if self.owner.piwave:
+            self.owner.piwave.cleanup()
+
+        if self.owner.ws_client:
+            await self.owner.ws_client.disconnect()
+
+        self.owner.tips.stop()
+
+        Log.client("Client stopped")
+
+def setup(reg):
+    reg.register(ClientStopOp)
