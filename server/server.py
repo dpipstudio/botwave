b2cf3f7d8da88dd185bef26d2140e8597361cf96
douxxtech <douxx@douxx.tech>
2026-09-03 18:02:33 +0200

    [server] server.py: fixed copy-paste error in comment

--- server/server.py
+++ server/server.py
@@ -312,7 +312,7 @@ async def main():
         Env.set("WAIT_START", str(True))  
 
     server = BotWaveServer()
-    server.registry.from_dir(Path(__file__).resolve().parent / "ops")                   # local/ops
+    server.registry.from_dir(Path(__file__).resolve().parent / "ops")                   # server/ops
     server.registry.from_dir(Path(__file__).resolve().parent.parent / "shared" / "ops") # shared/ops
 
 
