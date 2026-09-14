74523ba7a4cab177bd73683747900b881758eebb
douxxtech <douxx@douxx.tech>
2026-09-14 11:24:22 +0200

    [local] ops/handlers.py: removed HandlersCliOp that now is in shared/ops/handlers.py

--- local/ops/handlers.py
+++ local/ops/handlers.py
@@ -1,47 +1,10 @@
 import os
 from typing import Any
 
-from shared.dirutils import BW_PATH
 from shared.env import Env
 from shared.logger import Log
-from shared.ops import CliOp, GeneralOp
+from shared.ops import GeneralOp
 from shared.protocol import PROTOCOL_VERSION
-
-class HandlersCliOp(CliOp):
-    """
-    The 'handlers' command OP. Lists current handlers if no
-    argument provided, or displays the content of an handler file
-    if the file exists. Overall just a HandlerExecutor wrapper.
-    """
-
-    name = "handlers"
-    syntax = "[filename]"
-    short_help = "List all handlers and custom commands available"
-    long_help = """\
-Lists all handlers (.hdl and .shdl) and custom commands
-(.cmd) files in the handlers directory.
-If a [filename] is provided, displays the content of that file.
-"""
-    examples = [
-        "handlers",
-        "handlers hello.cmd"
-    ]
-    env_vars = {
-        "HANDLERS_DIR": (f"{BW_PATH}/handlers", "The directory handlers are located into")
-    }
-
-    async def handle(self, file: str = "", is_cmd: bool = False, cmd_parts: list[str] = []):
-        if is_cmd:
-            file = self.parse(cmd_parts)
-
-        if file:
-            self.owner.handlers_executor.list_handler_commands(file)
-
-        else:
-            self.owner.handlers_executor.list_handlers()
-
-    def parse(self, cmd_parts: list[str]) -> Any:
-        return cmd_parts[0] if len(cmd_parts) > 0 else None
 
 class HandlersEventsOp(GeneralOp):
     """
@@ -124,5 +87,4 @@ class HandlersEventsOp(GeneralOp):
 
         
 def setup(reg: Any):
-    reg.register(HandlersCliOp)
     reg.register(HandlersEventsOp)
